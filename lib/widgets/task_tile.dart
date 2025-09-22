e03d5e78688e0d6a021c40b669e8d7d50bbca2ee
Halema <halema.alaqily@globant.com>
2025-09-22 16:19:07 +0300

    Refactor TaskTile to lift checkbox state and use final fields

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({super.key});
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void toggleCheckbox(bool? value) {
+    setState(() {
+      isChecked = value ?? false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("data"),
-      trailing: Checkbox(value: false, onChanged: (bool? value) {}),
+      title: const Text("data"),
+      trailing: TaskCheckBox(isChecked: isChecked, onChanged: toggleCheckbox),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool isChecked;
+  final Function(bool?) onChanged;
+
+  const TaskCheckBox({
+    super.key,
+    required this.isChecked,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: onChanged,
     );
   }
 }
