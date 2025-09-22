b9cc791df115a30404575a523754c559c6105b0e
Halema <halema.alaqily@globant.com>
2025-09-22 12:54:09 +0300

    feat: add floating action button and task container to Home screen

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -7,44 +7,59 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(
-          top: 60.0,
-          left: 30.0,
-          right: 30.0,
-          bottom: 30.0,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 30.0,
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-                color: Colors.lightBlueAccent,
-              ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          print('Add button pressed');
+        },
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(30, 60, 30, 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
             ),
-            SizedBox(height: 5),
-            Text(
-              'Todoey',
-              style: TextStyle(
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: const BoxDecoration(
                 color: Colors.white,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
               ),
             ),
-            Text(
-              '12 tasks',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-            Container(
-              height: 300,
-              decoration: BoxDecoration(color: Colors.white),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
