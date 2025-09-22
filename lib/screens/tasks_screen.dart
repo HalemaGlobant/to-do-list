f3e722b34e87490cd575d178867ced5a1c5ab195
Halema <halema.alaqily@globant.com>
2025-09-22 12:22:02 +0300

    feat: build initial Home screen UI

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60.0,
+          left: 30.0,
+          right: 30.0,
+          bottom: 30.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 30.0,
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Todoey',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              '12 tasks',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            Container(
+              height: 300,
+              decoration: BoxDecoration(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
