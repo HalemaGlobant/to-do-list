5d3923a41108afaa89105bb657521ea7cde7936b
Halema <halema.alaqily@globant.com>
2025-09-24 13:12:34 +0300

    add calendar api

---
+++ lib/service/calendar/holiday_assets.dart
@@ -0,0 +1,21 @@
+class HolidayAssets {
+  final String imagePath;
+  final String announcement;
+
+  HolidayAssets({required this.imagePath, required this.announcement});
+}
+
+final Map<String, HolidayAssets> holidayAssets = {
+  "Saudi National Day": HolidayAssets(
+    imagePath: 'assets/images/ksa.avif',
+    announcement: "🇸🇦 Happy Saudi National Day!",
+  ),
+  "New Year's Day": HolidayAssets(
+    imagePath: 'assets/images/new_year.png',
+    announcement: "🎉 Happy New Year!",
+  ),
+  "Rosh Hashana": HolidayAssets(
+    imagePath: 'assets/images/Rosh-Hashanah.webp',
+    announcement: "Happy Rosh Hashana Day!",
+  ),
+};
